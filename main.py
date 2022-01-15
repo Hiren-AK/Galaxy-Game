099d3559427bd634a6e3266e59f12abc753199dc
unknown <hirenarvind2013@gmail.com>
2022-01-15 13:44:23 +0400

    skeleton code

---
+++ main.py
@@ -0,0 +1,15 @@
+from re import L
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+class MainWidget(Widget):
+    pass
+
+class GalaxyGame(App):
+    pass
+
+def main():
+    GalaxyGame().run()
+  
+if __name__=="__main__":
+    main()
