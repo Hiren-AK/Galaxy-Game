680a53249e7b904b743851026c26acccb087aa8c
unknown <hirenarvind2013@gmail.com>
2022-01-15 15:27:24 +0400

    added the horizontal lines, but perspective correction needed for horizontal lines

--- main.py
+++ main.py
@@ -8,18 +8,23 @@ class MainWidget(Widget):
     perspectivePointX = NumericProperty(0)
     perspectivePointY = NumericProperty(0)
     V_num_lines = 6
-    V_spacing_lines = 0.2       #10% of screen width
+    V_spacing_lines = 0.1       #10% of screen width
     vertical_lines = []
+    H_num_lines = 9
+    H_spacing_lines = 0.1       #10% of screen height
+    horizontal_lines = []
 
     def __init__(self, **kwargs):
         super(MainWidget, self).__init__(**kwargs)
         self.init_vertical_lines()
+        self.init_horizontal_lines()
     
     def on_parent(self, widget, parent):
         pass
 
     def on_size(self, *args):
         self.update_verticle_lines()
+        self.update_horizontal_lines()
 
     def init_vertical_lines(self):
         with self.canvas:
@@ -37,6 +42,24 @@ class MainWidget(Widget):
             x2, y2 = self.transform(x_line, self.height)
             self.vertical_lines[i].points = [x1, y1, x2, y2]
             offset += 1
+
+    def init_horizontal_lines(self):
+        with self.canvas:
+            Color(1, 1, 1)
+            for i in range(0, self.H_num_lines):
+                self.horizontal_lines.append(Line())
+
+    def update_horizontal_lines(self):
+        x_center_line = self.width/2
+        offset = -int(self.V_num_lines/2)+0.5
+        spacing = self.V_spacing_lines * self.width
+        x_min = x_center_line + (offset*spacing)
+        x_max = x_center_line - (offset*spacing)
+        for i in range(self.H_num_lines):
+            y_line = 0 + i*spacing
+            x1, y1 = self.transform(x_min, y_line)
+            x2, y2 = self.transform(x_max, y_line)
+            self.horizontal_lines[i].points = [x1, y1, x2, y2]
 
     def transform(self, x, y):
         #return self.transform2D(x, y)
